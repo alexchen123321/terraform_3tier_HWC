c920d0176d06a21f8d798fc85aa59c4cb89abaf2
Chendong(AlexC84266076Cloud) <c84266076@china.huawei.com>
2023-03-10 16:14:06 +0800

    fix

--- ecs_test/provider.tf
+++ ecs_test/provider.tf
@@ -1,5 +1,4 @@
 provider "huaweicloud" {
   region     = module.global_vars.default_region
-  access_key = module.global_vars.common_service_account_ak
-  secret_key = module.global_vars.common_service_account_sk
+
 }
