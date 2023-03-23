c70cf78c025a3c2a57a3228099e44f3783ecf1a0
Chendong(AlexC84266076Cloud) <c84266076@china.huawei.com>
2023-03-23 17:16:23 +0800

    mock AK SK

--- 3tier/provider.tf
+++ 3tier/provider.tf
@@ -1,8 +1,8 @@
 
 provider "huaweicloud" {
 
-  region     = "cn-south-1"
-  access_key = "fxxRABCsddddd123ddxx"  # mock AK
-  secret_key = "Ad125785xx6Isdrf11DGFGfrghghhmFFAfgdTBxx" # mock SK
+  # region     = "cn-south-1"
+  # access_key = "fxxRABCsddddd123ddxx"  # mock AK
+  # secret_key = "Ad125785xx6Isdrf11DGFGfrghghhmFFAfgdTBxx" # mock SK
 
 }
